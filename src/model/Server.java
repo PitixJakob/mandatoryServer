742d170ea612a3cfda07472a679e1df5c88dcbf2
Jakob Ferdinandsen <jakob@jhaaf.dk>
2016-09-12 11:22:58 +0200

    Cleaned up server and added Javadoc

--- src/model/Server.java
+++ src/model/Server.java
@@ -12,8 +12,81 @@ public class Server implements Runnable {
     private ArrayList<User> users;
     private ServerSocket serverSocket;
 
-    public Server() throws IOException {
+    /**
+     * Initial server setup
+     * @throws IOException
+     */
+    public Server() {
         users = new ArrayList<>();
+    }
+
+    /**
+     * Sends a message to all user
+     * @param message
+     */
+    public void broadcast(String message) {
+        System.out.println(message);
+
+        for (User user : users) {
+            user.receiveMessage(message);
+        }
+    }
+
+    /**
+     * Checks if User is already on the list, if so, returns false, if not, returns true and adds the user to the list
+     * @param user
+     * @return if successful, returns true
+     */
+    public boolean addUser(User user){
+        boolean result = true;
+
+        //Checks if user is already on the list
+        for (User listUser : users) {
+            if (user.getUsername().equals(listUser.getUsername())){
+                result = false;
+            }
+        }
+
+        //If user is not on the list, adds user to the users list and sends "J_OK" to user
+        if (result){
+            users.add(user);
+            broadcast("DATA User: "+user.getUsername()+" joined the chat");
+            user.receiveMessage("J_OK");
+            updateList();
+            return true;
+        }else{ // If user is on the list, server responds with "J_ERR"
+            user.receiveMessage("J_ERR");
+            return false;
+        }
+    }
+
+    /**
+     * removes a user from the list of connected users
+     * @param user
+     */
+    public void removeUser(User user){
+        User userToBeRemoved = null;
+        for (User listUser : users) {
+            if (listUser.getUsername().equals(user.getUsername())){
+                userToBeRemoved = listUser;
+            }
+        }
+        if (userToBeRemoved != null){
+            broadcast("DATA User: "+userToBeRemoved.getUsername()+" disconnected");
+            users.remove(userToBeRemoved);
+            updateList();
+        }
+    }
+
+    /**
+     * Updates connected users list and broadcasts the update
+     */
+    public void updateList(){
+        String list = "LIST";
+        for (User user : users) {
+            list += " "+user.getUsername();
+        }
+        broadcast(list);
     }
 
     @Override
@@ -33,54 +106,5 @@ public class Server implements Runnable {
         }
     }
 
-    public void broadcast(String message) {
-        System.out.println(message);
-
-        for (User user : users) {
-            user.receiveMessage(message);
-        }
-    }
-
-    public boolean addUser(User user){
-        boolean result = true;
-
-        for (User listUser : users) {
-            if (user.getUsername().equals(listUser.getUsername())){
-                result = false;
-            }
-        }
-        if (result){
-            users.add(user);
-            broadcast("DATA User: "+user.getUsername()+" joined the chat");
-            user.receiveMessage("J_OK");
-            updateList();
-            return true;
-        }else{
-            user.receiveMessage("J_ERR");
-            return false;
-        }
-    }
-
-    public void removeUser(User user){
-        User userToBeRemoved = null;
-        for (User listUser : users) {
-            if (listUser.getUsername().equals(user.getUsername())){
-                userToBeRemoved = listUser;
-            }
-        }
-        if (userToBeRemoved != null){
-            broadcast("DATA User: "+userToBeRemoved.getUsername()+" disconnected");
-            users.remove(userToBeRemoved);
-            updateList();
-        }
-    }
-
-    public void updateList(){
-        String list = "LIST";
-        for (User user : users) {
-            list += " "+user.getUsername();
-        }
-        broadcast(list);
-    }
 
 }
